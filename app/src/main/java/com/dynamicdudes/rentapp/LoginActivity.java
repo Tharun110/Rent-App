fb7041aaf2153d04b46036b9c92758afea67a47c
Tharun110 <72254564+Tharun110@users.noreply.github.com>
2020-12-15 13:39:16 +0530

    Email Registration

---
+++ app/src/main/java/com/dynamicdudes/rentapp/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.dynamicdudes.rentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText mail,password;
+    TextView register;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mail = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String validemail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                String validpwd = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+                String email = mail.getText().toString();
+                String pwd = password.getText().toString();
+
+                Matcher matcherMail = Pattern.compile(validemail).matcher(email);
+                Matcher matcherPwd = Pattern.compile(validpwd).matcher(pwd);
+
+                if (matcherMail.matches() && matcherPwd.matches()){
+                    Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Enter Valid email and password",Toast.LENGTH_LONG).show();
+                }
+                password.setError("Enter Password");
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
